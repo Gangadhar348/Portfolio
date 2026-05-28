140a8c38191c26f5dbbbb0c282389c311768e8ae
Gangadhar348 <nagathangangadhar03@gmail.com>
2026-05-29 02:57:17 +0530

    Added HTML CSS JS project

---
+++ Portfilo/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend", "Youtuber", "Web Developer"], // ✅ lowercase 's', fixed "Forntend"
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
